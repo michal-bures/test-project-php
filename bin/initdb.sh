f3954aaa104580e31b192094e2c39bef42be759a
michal-bures <embaircz@gmail.com>
2023-04-07 15:28:17 +0200

    dev environment setup, script for populating database

---
+++ bin/initdb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Initializes database schema and adds default records
+# - ASSUMES mysql container was already started with docker-compose
+# - Will fail if database already exists
+
+set -e
+SCRIPT_DIR=$( dirname "$0"; )
+ROOT_DIR="${SCRIPT_DIR}/.."
+
+source "${ROOT_DIR}/.env"
+
+SQL_SCRIPT="
+   CREATE DATABASE ${MYSQL_DATABASE};
+   USE ${MYSQL_DATABASE}
+   $( cat "${SCRIPT_DIR}/../database/schema.sql")
+"
+
+echo "${SQL_SCRIPT}" | docker-compose exec -T mysql mysql -u root --password="${MYSQL_PASSWORD}"
+
+echo "Database schema and content should be ready!"
